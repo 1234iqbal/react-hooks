cecff3cae184d18015b456c9dae5b0bc4ba94bcf
user <mifjak1@gmail.com>
2019-07-26 21:29:20 +0700

    react hooks

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "../../asset/img/logo/logo.svg";
+import "./App.css";
 
 function App() {
   return (
